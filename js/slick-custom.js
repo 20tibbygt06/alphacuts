7506640fd3814081f2b3d19d0b507e4d9533914e
Luis Aguilar <luis.e.aguilar@outlook.com>
2017-07-03 12:38:21 -0500

    update

--- js/slick-custom.js
+++ js/slick-custom.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
     $('.senior-slick').slick({
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      pauseOnFocus: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+
     });
 });
 
 $(document).ready(function() {
     $('.cordless-slick').slick({
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      pauseOnFocus: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+
     });
 });
+
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+}
