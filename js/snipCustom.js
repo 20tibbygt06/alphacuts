1f702449e09d811bc417e679064f47af581ad4ed
Luis Aguilar <20tibbygt06@users.noreply.github.com>
2017-07-04 09:21:56 -0500

    update

--- js/snipCustom.js
+++ js/snipCustom.js
@@ -1,1 +1,6 @@
 Snipcart.api.configure('show_continue_shopping', true);
+
+Snipcart.execute('bind', 'order.completed', function(order) {
+  var html = $("#cart-content-text").html();
+  $(html).insertAfter($("#snipcart-header"));
+});
